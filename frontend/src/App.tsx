a3cfc0ce60c9c3c869a87249f6d9733d4a47a023
John Paul Banera <johnpaul.banera@sun-asterisk.com>
2022-08-12 17:35:37 +0800

    Setup tailwind css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
-
+import logo from './assets/images/add.png'
 
 function App() {
   return (
-    <h1>hellow</h1>
+    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4 justify-center">
+      <p className="text-xl font-medium text-black ">Hello</p>
+    </div>
   );
 }
 
