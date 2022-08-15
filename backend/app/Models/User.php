39db38aa4e3f2823e90570104d429d1fbb617ec5
John Paul Banera <johnpaul.banera@sun-asterisk.com>
2022-08-15 12:35:04 +0800

    feature/userAPI | Update user model and migration

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -17,9 +17,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
+    protected $guarded = [
         'password',
     ];
 
