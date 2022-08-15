899ed38d362ceaef1f46f5bc22b341c07e3a4d54
John Paul Banera <johnpaul.banera@sun-asterisk.com>
2022-08-15 12:35:33 +0800

    feature/userAPI | Create user factory and seeder

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -17,12 +17,16 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $fname = $this->faker->firstName();
+        static $id = 2;
+
         return [
-            'name' => fake()->name(),
-            'email' => fake()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'first_name' => $fname,
+            'last_name' => $this->faker->lastName(),
+            'avatar' => "https://api.multiavatar.com/" . $id++ . "/$fname",
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password 
+            'is_admin' => false
         ];
     }
 
---
+++ backend/database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'first_name' => 'Super',
+            'last_name' => 'Admin',
+            'avatar' => 'https://api.multiavatar.com/1/admin.png',
+            'email' => 'admin@super.com',
+            'password' => bcrypt('letmein'),
+            'is_admin' => true
+        ]);
+
+        User::factory(2)->create();
+    }
+}
